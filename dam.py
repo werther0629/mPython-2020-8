3f9a5aa0a556c8d06a62a1ddef9fb4e808472f3f
Werther <66673822+werther0629@users.noreply.github.com>
2020-08-11 16:39:50 +0800

    Add files via upload

---
+++ dam.py
@@ -0,0 +1,13 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+x,y,z = mc.player.getPos()
+
+a = 0
+while a<20:
+    mc.setBlocks(x,y-1,z+30,x,y-10,z-30,19)
+    x = x-5
+    a = a+1
+    
+#dam
+    
+
---
+++ flower.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 10:59:30 2020
+
+@author: user
+"""
+
+
